17346e1fa3183154b35c2ccdde4f905b50604d4c
Gonzalo Medina <gonzalo@bigcartel.com>
2024-11-14 11:45:10 -0700

    fix: change social image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/ctfl-card.jpg',
     navbar: {
       title: 'Contentful Documentation',
       logo: {
