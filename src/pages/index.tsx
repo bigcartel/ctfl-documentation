a0dc73ba4bfd40f95f18fea29881e880a3219d0e
Gonzalo Medina <gonzalo@bigcartel.com>
2024-11-13 20:10:42 -0700

    fix: apply redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to='/docs/intro' />;
+  return (
+    <Redirect
+      to={`${
+        process.env.NODE_ENV === 'production' ? '/ctfl-documentation' : ''
+      }/docs/intro`}
+    />
+  );
 }
