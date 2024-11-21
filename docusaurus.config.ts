75000117e9256ada79880998cc935cb1f70989b6
Gonzalo Medina <gonzalo@bigcartel.com>
2024-11-20 21:52:33 -0700

    fix: create structure

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,30 +2,17 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'Contentful Big Cartel Documentation',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://bigcartel.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.NODE_ENV === 'production' ? '/ctfl-documentation/' : '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'bigcartel', // Usually your GitHub org/user name.
-  projectName: 'ctfl-documentation', // Usually your repo name.
+  organizationName: 'bigcartel',
+  projectName: 'ctfl-documentation',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -46,7 +33,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Contentful Documentation',
